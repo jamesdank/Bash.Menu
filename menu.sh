041639a87e7f35634261dc2b18924897d6d98228
jamesdank <94645949+jamesdank@users.noreply.github.com>
2024-01-01 19:25:06 -0600

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -15,3 +15,40 @@ function color_green () {
 function color_blue () {
     echo -ne $blue$1$clear
 }
+
+function menu () {
+
+    echo -ne "
+    Menu
+    $(color_green '1)') Example 1
+    $(color_green '2)') Example 2
+    $(color_green '3)') Example 3
+    $(color_green '0)') Exit
+    $(color_blue 'Choose an option:') "
+
+    read A
+            
+        case $A in
+
+        1) 
+        command 1 ;
+        command 2 ;
+        last command ;;
+
+        2) 
+        command 1 ;
+        command 2 ;
+        last command ;;
+
+        3)
+        command 1 ;
+        command 2 ;
+        last command ;;
+
+        0) 
+        exit 0 ;;
+        
+        *) 
+        echo -e "${red}Wrong option${nc}" ;;
+        esac
+}
